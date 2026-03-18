1162a06d6406d3c6650bee2508dd199419fe7ac5
Angee <ajco14987@gmail.com>
2026-03-18 13:16:47 +0100

    Pasa el commit de CLientes que se encuentra en nengocio

---
+++ src/main/java/org/iesalandalus/programacion/tallermecanico/modelo/negocio/Clientes.java
@@ -0,0 +1,68 @@
+package org.iesalandalus.programacion.tallermecanico.modelo.negocio;
+
+import org.iesalandalus.programacion.tallermecanico.modelo.TallerMecanicoExcepcion;
+import org.iesalandalus.programacion.tallermecanico.modelo.dominio.Cliente;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+public class Clientes {
+        private final List<Cliente> coleccionClientes;
+
+        public Clientes(){
+            this.coleccionClientes = new ArrayList<>();
+        }
+
+        public List<Cliente> get(){
+            return coleccionClientes;
+        }
+
+        public void insertar(Cliente cliente) throws TallerMecanicoExcepcion {
+            Objects.requireNonNull(cliente, "No se puede insertar un cliente nulo.");
+            if (coleccionClientes.contains(cliente)) {
+                throw new TallerMecanicoExcepcion("Ya existe un cliente con ese DNI.");
+            }
+            coleccionClientes.add(cliente);
+        }
+
+        public Cliente modificar(Cliente cliente, String nombre, String telefono) throws TallerMecanicoExcepcion{
+            Objects.requireNonNull(cliente, "No se puede modificar un cliente nulo.");
+
+            int indice = coleccionClientes.indexOf(cliente);
+            if (indice == -1) {
+                throw new TallerMecanicoExcepcion("No existe ningún cliente con ese DNI.");
+            }
+
+            Cliente clienteModificado = coleccionClientes.get(indice);
+            if (nombre != null && !nombre.isBlank()) {
+                clienteModificado.setNombre(nombre);
+            }
+            if (telefono != null && !telefono.isBlank()) {
+                clienteModificado.setTelefono(telefono);
+            }
+
+            return clienteModificado;
+        }
+
+        public Cliente buscar(Cliente cliente){
+            Objects.requireNonNull(cliente, "No se puede buscar un cliente nulo.");
+
+            int indice = coleccionClientes.indexOf(cliente);
+            if (indice != -1) {
+                return coleccionClientes.get(indice);
+            }
+            return null;
+        }
+
+        public void borrar(Cliente cliente) throws TallerMecanicoExcepcion {
+            Objects.requireNonNull(cliente, "No se puede borrar un cliente nulo.");
+
+            boolean borrado = coleccionClientes.remove(cliente);
+            if (!borrado) {
+                throw new TallerMecanicoExcepcion("No existe ningún cliente con ese DNI.");
+            }
+        }
+}
+
+
